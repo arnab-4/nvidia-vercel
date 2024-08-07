458be536ec3e2ba6e93c245798fa648dd8bbfc15
Arnab Manna <107208391+arnab-4@users.noreply.github.com>
2024-08-07 18:07:28 +0530

    Update models.ts

--- lib/models.ts
+++ lib/models.ts
@@ -4,10 +4,10 @@ export const models = [
   "google/gemma-2-27b-it",
   "meta/llama3-8b-instruct",
   "meta/llama3-70b-instruct",
-  "meta/llama-3.1-405b-instruct",
+  // "meta/llama-3.1-405b-instruct",
   "nvidia/llama3-chatqa-1.5-8b",
   "nvidia/llama3-chatqa-1.5-70b",
   "nvidia/nemotron-4-340b-instruct",
   "ibm/granite-8b-code-instruct",
-  "ibm/granite-34b-code-instruct",
+  // "ibm/granite-34b-code-instruct",
 ];
